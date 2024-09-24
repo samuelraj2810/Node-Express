206474f0764612a5663e17827d11272d4b33133f
samuel2810 <rja54321@gmail.com>
2024-09-24 08:25:35 +0530

    Hashing the Password and Sending the Original Password to the user through Email

--- Src/Controller/Register.Controller.js
+++ Src/Controller/Register.Controller.js
@@ -1,9 +1,11 @@
 const ModelReg = require("../Model/Register.Model");
 const GeneratePassword = require("../Utils/GeneratePassword")
+const Bcrypt = require("bcrypt");
+const SendMail = require("../Utils/SendEmail");
 
 const RegController = async (req, res) => {
   try {
-    let { Email, ...resBody } = req.body
+    let { Email, Name } = req.body
     const CheckEmail = await ModelReg.findOne({Email})
     if(CheckEmail) {
       return res.status(409).json({Message: "Email already exists...!!"})
@@ -11,14 +13,19 @@ const RegController = async (req, res) => {
 
     let Password = GeneratePassword(8)
     console.log(Password);
+
+    let HashPassword = await Bcrypt.hash(Password, 10)
     
 
     let data = {
       ...req.body,
-      Password,
+      Password: HashPassword,
       Created: "Success"
     }
     const Register = await ModelReg.create(data);
+
+     await SendMail(Email, Name, Password)
+
     res.json({
       Register,
       message: "Registration Successful",
--- Src/Utils/SendEmail.js
+++ Src/Utils/SendEmail.js
@@ -1,1 +1,29 @@
+const NodeMailer = require("nodemailer");
 
+const SendMail = async(Email, Name, Password) => {
+  try {
+    let Transporter = NodeMailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.Email,
+        pass: process.env.Pass,
+      },
+    });
+
+    let MailSchema = {
+      from: process.env.Email,
+      to: Email,
+      subject: `Welcome to our Club.`,
+      text: `Hello ${Name}.This is your Password: ${Password}`,
+    };
+
+    await Transporter.sendMail(MailSchema)
+    console.log(("Mail Sent Successfully..."));
+    
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    
+  }
+};
+
+module.exports = SendMail;
